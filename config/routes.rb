e70c132a667307d289ca83087ba55bb2b92c8fa3
Jason Zhao <jzhao.690@gmail.com>
2016-09-01 14:30:53 -0400

    migrate to shopify_app 7.2.0, bump shopify_api version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount ShopifyApp::Engine, at: '/'
   controller :sessions do
     get 'login' => :new, :as => :login
     post 'login' => :create, :as => :authenticate

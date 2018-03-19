da8657493f99dfcd7c86b17b37a3ddfec2e65f34
Leanne Shapton <leanne.shapton@shopify.com>
2018-03-18 20:33:21 -0400

    configure for locally

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,9 @@ ShopifyApp.configure do |config|
   config.application_name = 'Billing Demo App'
   config.api_key = ENV["BILLING_DEMO_API_KEY"]
   config.secret = ENV["BILLING_DEMO_SECRET_KEY"]
+  config.myshopify_domain = 'myshopify.io'
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
+
+ShopifyAPI::Session.myshopify_domain = ShopifyApp.configuration.myshopify_domain

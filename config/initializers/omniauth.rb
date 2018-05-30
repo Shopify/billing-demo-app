02113a78ea6636ae285abc54d7f3c4ad89869c0d
Leanne Shapton <leanne.shapton@shopify.com>
2018-05-30 10:56:42 -0400

    configure for locally

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,11 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
-    scope: ShopifyApp.configuration.scope,
-    setup: lambda { |env|
-      params = Rack::Utils.parse_query(env["QUERY_STRING"])
-      site_url = "https://#{params["shop"]}"
-      env["omniauth.strategy"].options[:myshopify_domain] = ShopifyApp.configuration.myshopify_domain
-      env["omniauth.strategy"].options[:client_options][:site] = site_url
-    }
+    scope: ShopifyApp.configuration.scope
 end
